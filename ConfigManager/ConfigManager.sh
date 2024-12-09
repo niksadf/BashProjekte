98534c59c61202cb30fcf5cc51cd7781d6c0a866
niksadf <160512286+niksadf@users.noreply.github.com>
2024-12-09 10:33:23 +0100

    Added remarks to ConfigManager.sh

--- ConfigManager/ConfigManager.sh
+++ ConfigManager/ConfigManager.sh
@@ -3,11 +3,13 @@
 clear
 CONFIG_FILE="/etc/FirmaXYZ.conf"
 
+#Testet ob als Root ausgeführt wurde:
 if [ "$EUID" -ne 0 ]; then
     echo "Bitte als Root ausführen."
     exit 1
 fi
 
+#Zeigt die optionen and und fangt den Loop an
 while true; do
     echo "Was möchten Sie tun?"
     echo "1) Konfigurationsdatei erstellen"
@@ -16,38 +18,45 @@ while true; do
     echo "4) Beenden"
     read -p "Wählen Sie eine Option (1-4): " choice
 
+#Erstellt die Konfigurationsdatei
     case $choice in
         1)
             echo "key=value" > "$CONFIG_FILE"
             clear
             echo "Konfigurationsdatei erstellt: $CONFIG_FILE"
             ;;
+# Liest die Konfigurationsdatei vor
         2)
             if [ -f "$CONFIG_FILE" ]; then
             clear
                 cat "$CONFIG_FILE"
                 echo ""
+# Fehlermeldung, Datei exestiert nicht, kann nicht vorgelesen werden.
             else
             clear
                 echo "Konfigurationsdatei existiert nicht."
             fi
             ;;
+# Löscht die Konfigurationsdatei
         3)
             if [ -f "$CONFIG_FILE" ]; then
             clear
                 rm "$CONFIG_FILE"
                 echo "Konfigurationsdatei gelöscht: $CONFIG_FILE"
+# Fehlermeldung, Datei exestiert nicht, kann nicht gelöscht werden.
             else
             clear
                 echo "Konfigurationsdatei existiert nicht."
             fi
             ;;
+# Beendet das Programm
         4)
             echo "Beenden..."
             sleep 1
             exit 0
             ;;
-        *)
+# Fehlermeldung für ungültige Nummern
+       *)
             clear
             echo "Ungültige Auswahl. Bitte erneut versuchen."
             ;;
